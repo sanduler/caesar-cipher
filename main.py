a66a8a9bda5968b53bca90e114874e284519ba52
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-19 13:13:36 -0800

    added the if statement to check user input

--- main.py
+++ main.py
@@ -39,16 +39,40 @@ def encrypt(plain_text, shift_amount):
 
 # TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-  # TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
-  # e.g.
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  # print output: "The decoded text is hello"
+
+def decrypt(plain_text, shift_amount):
+    cypher_text = ""
+    for letter in plain_text:
+        # determine the position of the letter in the alphabet
+        position = alphabet.index(letter)
+        # determine the position of the letter after the shift amount is acounted for
+        new_position = position - shift_amount
+        # determine a new position if the position is greater than the amount of letters
+        if new_position < 0:
+            # acounted for the index[0]
+            new_position = 26 - new_position
+        # find the new letter
+        new_letter = alphabet[new_position]
+        # add to the black cypher text
+        cypher_text += new_letter
+    # print the cipher text
+    print(f"The encoded text is {cypher_text}")
+
+# TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+# e.g.
+#cipher_text = "mjqqt"
+#shift = 5
+#plain_text = "hello"
+# print output: "The decoded text is hello"
 
 
 # TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
 
 # Calls the encrypt function and pass in the user inputs. Be able to test the code and encrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+if direction == 'encode':
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == 'decode':
+    decrypt(plain_text=text, shift_amount=shift)
+else:
+    print("Unknown direction, use encrypt or decrypt")
