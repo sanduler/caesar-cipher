e7155e8245ed6988c180f80b70bafa26fdc2da66
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-19 14:37:58 -0800

    added the logo

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # Date: January 17, 2022
 # Discription:
 
+from logo import background
+print(background)
+
 # list of the letters in the alphabet
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -18,6 +21,11 @@ def caesar(plain_text, shift_amount, cipher_direction):
         # create a negative shift for decode
         shift_amount *= -1
     for letter in plain_text:
+        # TODO-3: if the user enters a number/symbol/space?
+        # fix the code to keep the number/symbol/space when the text is encoded/decoded?
+        #e.g. start_text = "meet me at 3"
+        #end_text = "•••• •• •• 3"
+
         # determine the position of the letter in the alphabet
         position = alphabet.index(letter)
         # determine the position of the letter in the alphabet then add the shift amount
@@ -28,6 +36,18 @@ def caesar(plain_text, shift_amount, cipher_direction):
         # find the new letter
         cypher_text += alphabet[new_position]
     print(f"The {cipher_direction} text is {cypher_text}")
+
+# TODO-1: Import and print the logo from background.py when the program starts.
+
+# TODO-4: Figure out a way to ask the user if they want to restart the cipher program?
+# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+
+# TODO-2: If the user enters a shift that is greater than the number of letters in the alphabet?
+# Try running the program and entering a shift number of 45.
+# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+# Hint: Think about how you can use the modulus (%)
 
 
 caesar(plain_text=text, shift_amount=shift, cipher_direction=direction)
