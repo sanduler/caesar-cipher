17214c09e89862faa9275a0f758f8c7985f397ce
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-19 14:42:32 -0800

    fixed the boundry if shift is greater than 26

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+shift = shift % 26
 
 
 def caesar(plain_text, shift_amount, cipher_direction):
@@ -37,17 +38,9 @@ def caesar(plain_text, shift_amount, cipher_direction):
         cypher_text += alphabet[new_position]
     print(f"The {cipher_direction} text is {cypher_text}")
 
-# TODO-1: Import and print the logo from background.py when the program starts.
 
 # TODO-4: Figure out a way to ask the user if they want to restart the cipher program?
 # e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 # If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 # Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
-
-# TODO-2: If the user enters a shift that is greater than the number of letters in the alphabet?
-# Try running the program and entering a shift number of 45.
-# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-# Hint: Think about how you can use the modulus (%)
-
-
 caesar(plain_text=text, shift_amount=shift, cipher_direction=direction)
