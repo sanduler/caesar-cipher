f99d3547582915f4c125a2b19cdd8a44232c607b
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-19 13:26:26 -0800

    cleaned the code

--- main.py
+++ main.py
@@ -37,8 +37,6 @@ def encrypt(plain_text, shift_amount):
     # print the cipher text
     print(f"The encoded text is {cypher_text}")
 
-# TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-
 
 def decrypt(plain_text, shift_amount):
     cypher_text = ""
@@ -58,16 +56,6 @@ def decrypt(plain_text, shift_amount):
         cypher_text += new_letter
     # print the cipher text
     print(f"The encoded text is {cypher_text}")
-
-# TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
-# e.g.
-#cipher_text = "mjqqt"
-#shift = 5
-#plain_text = "hello"
-# print output: "The decoded text is hello"
-
-
-# TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
 
 # Calls the encrypt function and pass in the user inputs. Be able to test the code and encrypt a message.
